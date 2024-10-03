e6978c165889fdcac55abf4b1a0097ca3576edf8
Abdur Rahman Mahmud <ar7543078@gmail.com>
2024-10-03 23:49:52 +0600

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="border-t border-t-primary/20 footer footer-center sha   text-base-content p-4 fixed w-full z-20 bottom-0 end-0 backdrop-filter backdrop-blur-3xl ">
           <aside>
             <p>
-              Copyright © {new Date().getFullYear()} - All Right Reserved{" "}
+              Copyright © {new Date().getFullYear()} - All Right Reserved
               <a className="link ">by Abdur Rahman .</a>
             </p>
           </aside>
